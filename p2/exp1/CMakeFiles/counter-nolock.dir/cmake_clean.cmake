84c03cb42547e1cfab955f98068104693b138fde
Kur1su0 <1997weizice@gmail.com>
2020-10-14 05:56:40 +0000

    p2 exp2 wip

---
+++ p2/exp1/CMakeFiles/counter-nolock.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/counter-nolock.dir/counter-nolock.c.o"
+  "counter-nolock"
+  "counter-nolock.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/counter-nolock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
