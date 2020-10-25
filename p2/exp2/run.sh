2742a2211152d3a82613b63637e1a25e7d782790
Kur1su0 <1997weizice@gmail.com>
2020-10-25 00:15:38 +0000

    p2 exp2 wip

--- p2/exp2/run.sh
+++ p2/exp2/run.sh
@@ -23,8 +23,8 @@ run() {
   
   # TODO: Set thread counts to test here
   # for tr in 1 2 4 6 8 10 12 16 20
-  #for tr in 1 2 4 6 8
-  for tr in 1 2 3 4 5 6 7 8
+  for tr in 1 2 4 6 8
+  #for tr in {1..8}
 
   #for tr in 1 2
   do 
